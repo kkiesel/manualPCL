202f7f33294cb0fa838ff7af535673b164d83194
Knut Kiesel <kiesel@cern.ch>
2016-07-13 16:35:57 +0200

    write histogram only if result file avaiable

--- cleanUp.py
+++ cleanUp.py
@@ -12,11 +12,12 @@ if __name__ == "__main__":
     run = sys.argv[1]
     os.chdir("Results{}".format(run))
     log("Create histogram")
-    txtToHist("millepede.res", "pede.dump", "Run{}.root".format(run))
-    if triggerUpdate("millepede.res"):
-        if settings.mail:
-            sendMail(settings.mail, "Upload conditions", "Please upload conditions of Run {}".format(run))
-        subprocess.call(["uploadConditions.py", "TkAlignment.db"])
+    if os.path.exists("millepede.res"):
+        txtToHist("millepede.res", "pede.dump", "Run{}.root".format(run))
+        if triggerUpdate("millepede.res"):
+            if settings.mail:
+                sendMail(settings.mail, "Upload conditions", "Please upload conditions of Run {}".format(run))
+            subprocess.call(["uploadConditions.py", "TkAlignment.db"])
     os.chdir("..")
     log("Clean up")
     cleanUp(run)

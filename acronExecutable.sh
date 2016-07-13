a7061d4cea0a142bf92dad10082c3464983b08a7
Knut Kiesel <kiesel@cern.ch>
2016-07-13 14:43:36 +0200

    adding executable for acrom

---
+++ acronExecutable.sh
@@ -0,0 +1,12 @@
+logFile=/afs/cern.ch/user/k/kiesel/alignment/manualPCL/acron.log
+
+export SCRAM_ARCH=slc6_amd64_gcc453 >> $logFile 2>&1
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch >> $logFile 2>&1
+source $VO_CMS_SW_DIR/cmsset_default.sh >> $logFile 2>&1
+
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_12 >> $logFile 2>&1
+eval `scramv1 runtime -sh` >> $logFile 2>&1
+cd /afs/cern.ch/user/k/kiesel/alignment/manualPCL >> $logFile 2>&1
+python main.py >> $logFile 2>&1
+
+

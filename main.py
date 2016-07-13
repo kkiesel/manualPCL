84bd2400c2f5dcc74f3485a7209659a60eb8a045
Knut Kiesel <kiesel@cern.ch>
2016-07-13 15:37:30 +0200

    removing double function

--- main.py
+++ main.py
@@ -130,53 +130,6 @@ def getRunToProcess():
         sys.exit()
     return run, runInfo._sections[run]
 
-def writeNumberToFile(name, x):
-    vec = ROOT.TVectorF(1)
-    vec[0] = x
-    vec.Write(name)
-
-def txtToHist(inName, inName2, outName):
-    hists = [
-        ROOT.TH1F("Xpos", ";;#Delta x (#mum)", 6, 0, 6),
-        ROOT.TH1F("Ypos", ";;#Delta y (#mum)", 6, 0, 6),
-        ROOT.TH1F("Zpos", ";;#Delta z (#mum)", 6, 0, 6),
-        ROOT.TH1F("Xrot", ";;#Delta#theta_{x} (#murad)", 6, 0, 6),
-        ROOT.TH1F("Yrot", ";;#Delta#theta_{y} (#murad)", 6, 0, 6),
-        ROOT.TH1F("Zrot", ";;#Delta#theta_{z} (#murad)", 6, 0, 6)
-    ]
-    binLabels = ["FPIX(x+,z-)", "FPIX(x-,z-)", "BPIX(x+)", "BPIX(x-)", "FPIX(x+,z+)", "FPIX(x-,z+)"]
-    bins = [2726, 3212, 6, 878, 1752, 2238]
-    out = numpy.genfromtxt(inName, skip_header=1, skip_footer=48)
-    for objId, x, x, val, err in out:
-        objId = int(objId)
-        hist = objId%10-1
-        multiplier = 1e4 if hist in [0,1,2] else 1e6 # unit conversion to 'mu m' and 'mu rad'
-        val, err = float(val)*multiplier, float(err)*multiplier
-        bin = bins.index(objId/10) + 1
-        hists[hist].SetBinContent(bin, val)
-        hists[hist].SetBinError(bin, err)
-
-    with open(inName2) as f:
-        lines = f.readlines()
-        for iline, line in enumerate(lines):
-            if 'NREC =' in line:
-                nrec = float(line.split("=")[1])
-            m = re.match(".*Fraction of rejects =\s+([.\d])+\s*%.*", line)
-            frec = m.group(1) if m else -1.
-            if iline > 2 and "Sum(Chi^2)/Sum(Ndf)" in lines[iline-2]:
-                chi2 = float(line.split("=")[1])
-
-    f = ROOT.TFile(outName, "recreate")
-    for h in hists:
-        for ibinLabel, binLabel in enumerate(binLabels):
-            h.GetXaxis().SetBinLabel(ibinLabel+1, binLabel)
-        h.Write()
-    writeNumberToFile("chi2overNdf", chi2)
-    writeNumberToFile("numberOfRecords", nrec)
-    writeNumberToFile("fractionOfReject", frec)
-    f.Write()
-    f.Close()
-
 def writeExecutable(name, pyCfg, additional=[]):
     with open(name, "w") as f:
         f.write( "\n".join( ["#!/bin/sh",
